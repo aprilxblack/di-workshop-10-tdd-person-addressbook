81b5432372150fc42432a0552cdacc3de29f7a8b
Karolina Zdziarska <karolina.zdziarska1994@gmail.com>
2018-10-26 14:43:21 +0100

    completed the first task

--- models/person.js
+++ models/person.js
@@ -4,6 +4,7 @@ class Person {
         this.surname = surname;
         this.dob = dob;
         this.emails = [];
+        this.phoneNumbers = [];
     }
 
     fullname(){
@@ -12,6 +13,32 @@ class Person {
 
     addEmail(email){
         this.emails.push(email);
+    }
+
+    addPhoneNumber(phoneNumber){
+        this.phoneNumbers.push(phoneNumber);
+    }
+
+    returnFormattedDetails(){
+        var details = this.firstName + ' ' + this.surname + '\n'
+        + '----------' + '\n'
+        + 'DOB: ' + this.dob + '\n'
+        + '\n'
+        + 'Email Addresses:' + '\n'
+        var emailAdresses = ''
+        if(this.emails.length > 0){
+            for(var i = 0; i < this.emails.length; i++){
+                emailAdresses += '- ' + this.emails[i] + '\n'
+            }
+        }
+        var phones = ''
+        if(this.phoneNumbers.length > 0){
+            for(var number of this.phoneNumbers){
+                phones += '- ' + number + '\n'
+            }
+        }
+        
+        return details + emailAdresses + '\n' + 'Phone Numbers:' + '\n' + phones;
     }
 }
 
--- test/person.test.js
+++ test/person.test.js
@@ -11,6 +11,7 @@ describe('Person', ()=>{
         expect(person.surname).to.equal('Bloggs');
         expect(person.dob).to.equal('1 Jan 1990');
         expect(person.emails).to.deep.equal([]);
+        expect(person.phoneNumbers).to.deep.equal([]);
     })
     it('should capitalize the first name', ()=> {
         var person = new Person('amy', 'Nother', '1 Jan 1990');
@@ -30,6 +31,24 @@ describe('Person', ()=>{
 
         expect(person.emails).to.deep.equal(['joe@example.com', 'joe.bloggs@workexample.com'])
     })
+    it('should accept new phone numbers and add them to the array', ()=> {
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1990');
+
+        person.addPhoneNumber('123456789');
+        person.addPhoneNumber('987654321');
+
+        expect(person.phoneNumbers).to.deep.equal(['123456789', '987654321']);
+    })
+    it('should return a well formatted multi-line string', ()=> {
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1990');
+        
+        person.addEmail('joe@example.com');
+        person.addEmail('joe.bloggs@workexample.com');
+        
+        person.addPhoneNumber('123456789');
+        person.addPhoneNumber('987654321');
+
+        expect(person.returnFormattedDetails()).to.equal('Joe Bloggs' + '\n' + '----------' + '\n' + 'DOB: 1 Jan 1990' + '\n'+ '\n' + 'Email Addresses:' + '\n'+ '- joe@example.com' + '\n'+ '- joe.bloggs@workexample.com' + '\n'+ '\n'+ 'Phone Numbers:' + '\n'+ '- 123456789' + '\n'+ '- 987654321' + '\n');
+    })
     
 })
-// Your Person test goes here!
